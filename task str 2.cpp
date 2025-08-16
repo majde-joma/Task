11ccb7d3d5dccb8bfdb6c0cb3b4ca7a45c47d544
majde-joma <jmtm9688@gmail.com>
2025-08-16 18:29:01 +0300

    Create task str 2.cpp

---
+++ task str 2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>  
+using namespace std;
+int main()
+{
+string text;
+cout << "Enter a sentence: ";
+getline(cin, text);
+
+int count = 0;
+for (char c : text) {
+    c = tolower(c);
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+        count++;
+    }
+}
+
+cout << "Number of vowels in sentence: " << count << endl;
+
+
+
+
+  
+return 0;
+}
