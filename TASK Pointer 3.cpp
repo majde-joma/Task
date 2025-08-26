2c933d01b3fd77272ec2e58e2c05d7e594b9adb0
majde-joma <jmtm9688@gmail.com>
2025-08-26 16:06:47 +0300

    Create TASK Pointer 3.cpp

---
+++ TASK Pointer 3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <set>
+#include <algorithm>
+using namespace std;
+int main()
+{
+int N;
+cout << "Enter a number N: ";
+cin >> N;
+int* arr = new int[N];
+for (int i = 0; i < N; i++) {
+	arr[i] = (i + 1) * (i + 1);
+}
+cout << "First " << N << " square numbers: ";
+for (int i = 0; i < N; i++) {
+	cout << arr[i] << " ";
+}
+cout << endl;
+delete[] arr;
+ return 0;
+}
